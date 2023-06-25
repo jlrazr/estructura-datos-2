13669131a8b1372fd757c6115686a30de752b19f
Jose Luis Mora <jlrazr@users.noreply.github.com>
2023-06-25 00:07:33 -0600

    UI primeros pasos

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
+        // Inicia la interfaz de usuario
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Interfaz ui = new Interfaz();
+                ui.setVisible(true);
+            }
+        });
+
         Aplicacion aplicacion = new Aplicacion();
         Scanner scanner = new Scanner(System.in);
 
